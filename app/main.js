f3e2d103d584d4a5000e0ec020f6b16f1a261dd9
Virej Dasani <virej@Virejs-MacBook-Air.local>
2021-07-23 18:24:52 +0530

    Configure and initialise project

---
+++ app/main.js
@@ -0,0 +1,42 @@
+const { app, BrowserWindow } = require("electron");
+
+// const open = require('open')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 900,
+    height: 600,
+    minWidth: 680,
+    minHeight: 620,
+  });
+
+  // and load the index.html of the app.
+  win.loadFile("index.html");
+
+  // This opens all links with `target="_blank"` attribute in external browser
+  win.webContents.on("new-window", function (event, url) {
+    event.preventDefault();
+    open(url);
+  });
+
+  // Open the DevTools.
+  // win.webContents.openDevTools()
+}
+
+// When app is ready
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
